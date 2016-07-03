9a619cc204653f56436dff327bcfc6b5567e7879
Wolfgang <wolf@okwolf.com>
2016-07-03 09:00:15 -0700

    treat input as number instead of string

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,6 +1,6 @@
 $(function() {
   $('#inputNumber').change(function(event) {
-    var inputValue = event.target.value,
+    var inputValue = Number(event.target.value),
     outputValue = square(inputValue);
     $('#outputAnswer').text(outputValue);
   });
