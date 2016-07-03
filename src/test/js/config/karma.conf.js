875ee6519e3f1ddd6fef2900efabf7bd8061c223
Wolfgang <wolf@okwolf.com>
2016-07-03 04:37:22 -0700

    adding test coverage report

--- src/test/js/config/karma.conf.js
+++ src/test/js/config/karma.conf.js
@@ -5,15 +5,17 @@ module.exports = function(config) {
     config.set({
         frameworks: ['jasmine'],
         files: files,
+        preprocessors: { '../../src/main/webapp/js/**/*.js': ['coverage']},
         exclude: ['karma.conf*.js'],
-        reporters: ['dots'],
+        reporters: ['dots', 'coverage'],
         logLevel: config.LOG_INFO,
         browsers: ['PhantomJS'],
         singleRun: false,
         autoWatch: true,
         plugins: [
             'karma-jasmine',
-            'karma-phantomjs-launcher'
+            'karma-phantomjs-launcher',
+            'karma-coverage'
         ]
     });
 };
